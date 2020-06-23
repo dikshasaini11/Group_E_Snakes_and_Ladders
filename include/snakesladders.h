73b965ebe98a90fcbe9278ea9e8c1fc5da035878
Rupneet Kaur Gill <65581955+Rupneetkaurgill@users.noreply.github.com>
2020-06-23 15:45:48 +1000

    update snakesladders.h

--- include/snakesladders.h
+++ include/snakesladders.h
@@ -1,6 +1,34 @@
 #ifndef snakesladders_H
 #define snakesladders_H
 
+/**  @brief Function prototypes to move the cursor  to specific position
+* 
+* This Function is used to move the curser to specific position
+*
+* Input parameters are x-coordinate and y-coordinate
+*
+* This Function places curser at given coordinate position
+*
+*
+*/
+
+int locate(int x,int y);
+
+
+/** @brief Function prototypes to display the first screen of the game
+*
+* This Function is used to display the first screen of the game
+*
+* This Funtion takes the location in form of x,y coordinates
+*
+* This Function prints the name at given location
+*
+*
+*/
+
+void display_fstscr(void);
+
+
 /**  @brief Function prototypes to draw a game board
  *
  * This function is used to draw a 10x10 grid as a game board
